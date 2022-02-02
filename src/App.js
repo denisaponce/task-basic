3fdffc5bb1d56515f699e9e448c85ad0b62f8e10
Denisa Ponce <denisaponce2012@hotmail.com>
2022-02-02 10:14:58 -0300

    Modificado

--- src/App.js
+++ src/App.js
@@ -1,35 +1,37 @@
-import React, {useState } from 'react';
+
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
-  // crear una tarea
   const [task, setTask] = useState('');
-  //  Agregar nuevas tareas
   const [tasks, setTasks] = useState([]);
+
   const submitHandler = (e) => {
     e.preventDefault();
-    setTasks([...task, task]);
+    setTasks([...tasks, task]);
   };
 
   return (
-    <div className='Container'>
+    <div className='container'>
       <h1>Task Basic</h1>
-      <form onSubmit = {submitHandler}>
+      <form onSubmit={submitHandler}>
         <input
-          type="text"
-          placeholder='Ingrese una tarea'
+          type='text'
+          placeholder='Ingrese la tarea'
           value={task}
-          onChange={(e) => setTask(e.target.value)} />
+          onChange={(e) => setTask(e.target.value)}
+        />
+        <button type='submit'>Agregar Tarea</button>
       </form>
-      <div className='task-container'>
-        {task.map((items, index) => (
+      <div className='tasks-container'>
+        {tasks.map((item, index) => (
           <div key={index} className='task-card'>
-            {items}
+            {item}
           </div>
         ))}
       </div>
     </div>
-  )
+  );
 }
 
 export default App;
